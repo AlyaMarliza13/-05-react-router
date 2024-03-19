67577e3510d373ab3eea067f87dd11899b52d4b1
AlyaMarliza13 <alyamarlizakoesnanto03@gmail.com>
2024-03-20 02:31:33 +0700

    W05: Praktikum 1

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,14 @@
+export default function Profile() {
+    return (
+        <div>
+            <h1>Welcome to My Profile</h1> 
+            <h2>Selamat datang dan salam kenal</h2>
+            <p>Nama Lengkap: Alya Marliza Koesnanto</p>
+            <p>Nama Panggilan: Alya</p>
+            <p>Nomor Induk Mahasiswa: 2141720004</p>
+            <p>Nomor Absen: 03</p>
+            <p>Kelas: TI-3A</p>
+            <p>Asal: Kota Malang</p>
+        </div>
+    );
+  }
